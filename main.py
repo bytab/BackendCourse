e67027ac635e24b17c89e51fb556f46a796d7eed
bytab <spacefullpixel@gmail.com>
2025-03-10 00:36:37 +0500

    Задание №1: PUT и PATCH ручки отелей

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Query
+from fastapi import FastAPI, Body
 import uvicorn
 
 app = FastAPI()
@@ -9,12 +9,49 @@ hotels = [
     {"id": 3, "title": "Hotel C", "price": 300},
 ]
 
-
 @app.get("/hotels")
-def get_hotels():
-    return hotels
+def get_hotels(
+        title: str,
+):
+    return [hotel for hotel in hotels if hotel["title"] == title]
 
 
+@app.post("/hotels")
+def create_hotel(
+        title: str = Body(embed=True),
+):
+    global hotels
+    hotel = {"id": len(hotels) + 1, "title": title}
+    hotels.append(hotel)
+    return hotel
+@app.put("/hotels{id}")
+def update_hotel(
+        id: int,
+        title: str = Body(embed=True),
+):
+    hotel = next((hotel for hotel in hotels if hotel["id"] == id), None)
+    if hotel:
+        hotel["title"] = title
+        return hotel
+    return {"message": "Hotel not found"}
+@app.patch("/hotels{id}")
+def patch_hotel(
+        id: int,
+        title: str = Body(embed=True),
+):
+    hotel = next((hotel for hotel in hotels if hotel["id"] == id), None)
+    if hotel:
+        hotel["title"] = title
+        return hotel
+    return {"message": "Hotel not found"}
+
+@app.delete("/hotels{id}")
+def delete_hotel(id: int):
+    hotel = next((hotel for hotel in hotels if hotel["id"] == id), None)
+    if hotel:
+        hotels.remove(hotel)
+        return {"message": "Hotel deleted successfully"}
+    return {"message": "Hotel not found"}
 
 
 if __name__ == "__main__":
