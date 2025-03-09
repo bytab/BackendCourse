530ad30abec8ac4e6fb9995576319750b3393fa1
bytab <spacefullpixel@gmail.com>
2025-03-09 23:48:03 +0500

    Initial Commit

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Query
 import uvicorn
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"wrwrewr": "World"}
+hotels = [
+    {"id": 1, "title": "Hotel A", "price": 100},
+    {"id": 2, "title": "Hotel B", "price": 200},
+    {"id": 3, "title": "Hotel C", "price": 300},
+]
+
+
+@app.get("/hotels")
+def get_hotels(
+        title: str | None = Query(None, description="Hotel title"),
+):
+    return [hotel for hotel in hotels if hotel ["title"] == title]
+
+
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True, port=8002)
