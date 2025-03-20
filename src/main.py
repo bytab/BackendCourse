0c033ed05b146fd5c5840a496b90bfefb2deea9d
bytab <spacefullpixel@gmail.com>
2025-03-20 20:59:06 +0500

    Models&Migrations (db7021)

--- src/main.py
+++ src/main.py
@@ -5,9 +5,9 @@ import sys
 from pathlib import Path
 
 sys.path.append(str(Path(__file__).parent.parent))
-
 from src.api.hotels import router as router_hotels
-
+from src.config import settings
+print(f"{settings.DB_URL=}")
 app = FastAPI()
 
 app.include_router(router_hotels)
