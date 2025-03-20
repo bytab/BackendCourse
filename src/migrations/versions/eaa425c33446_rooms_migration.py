d198b0f5368aef69a1016938ccba0f7b684bdf67
bytab <spacefullpixel@gmail.com>
2025-03-20 21:38:30 +0500

    Rooms revisions

---
+++ src/migrations/versions/eaa425c33446_rooms_migration.py
@@ -0,0 +1,35 @@
+"""Rooms migration
+
+Revision ID: eaa425c33446
+Revises: 17c2f1cca804
+Create Date: 2025-03-20 21:31:56.532338
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'eaa425c33446'
+down_revision: Union[str, None] = '17c2f1cca804'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table('rooms',
+    sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('hotel_id', sa.Integer(), nullable=False),
+    sa.Column('title', sa.String(), nullable=False),
+    sa.Column('description', sa.String(), nullable=True),
+    sa.Column('price', sa.Integer(), nullable=False),
+    sa.Column('quantity', sa.Integer(), nullable=False),
+    sa.ForeignKeyConstraint(['hotel_id'], ['hotels.id'], ),
+    sa.PrimaryKeyConstraint('id')
+    )
+
+
+def downgrade() -> None:
+    op.drop_table('rooms')
